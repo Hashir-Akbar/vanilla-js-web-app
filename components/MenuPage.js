d31e865baf8e474987088e579d99d30db1a6b886
Hashir Akbar <hashirbusinessinfo@gmail.com>
2024-10-22 21:57:45 +0500

    learning about web components

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,19 @@
 export default class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: "open" });
+
+    const style = document.createElement("style");
+
+    this.root.appendChild(style);
+    async function styleGet() {
+      const data = await fetch("/components/MenuPage.css");
+      const content = await data.text();
+
+      style.appendChild(content);
+    }
+    styleGet()
   }
 
   connectedCallback() {
